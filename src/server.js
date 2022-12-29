ecda6912be9b8919de689608d0204ceeb01d4d5a
phat huynh <huynhtanphatleo@gmmail.com>
2022-12-29 22:51:35 +0700

    update CORS

--- src/server.js
+++ src/server.js
@@ -5,9 +5,34 @@ import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
 require("dotenv").config();
 let app = express();
+// Add headers before the routes are defined
+//khac phuc loi CORS
+// app.use(cors({ origin: true }));
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 //config app
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
